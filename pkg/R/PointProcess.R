98f894b45ce5768a774896b6f2fdb67d44b309d2
nielsrhansen <nielsrhansen@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-24 06:40:11 +0000

    Tidying code. New function 'setProcessData<-' implemented.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ppstat@34 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/PointProcess.R
+++ pkg/R/PointProcess.R
@@ -1,15 +1,46 @@
 setClass("PointProcess",
          representation(
-                        processDataEnv = "environment",  ### The processData as ProcessData is in this environment. Locked after initialization
-                        delta = "numeric",
-                        formula = "formula",
-                        family = "Family",
                         call = "call",
-                        support = "numeric",     ### A vector c(a,b) with the support, [a,b], of the g-functions
-                        Delta = "numeric",       ### The equidistant spacing between the g-evaluations
+
+                        ## The equidistant spacing between the g-evaluations.
+                        Delta = "numeric",      
+
+                        delta = "numeric",
+                        family = "Family",
+                        formula = "formula",
+
+                        ## Results from call to 'optim' goes here.
+                        optimResult = "list",
+                        
+                        ## The processData as ProcessData is in this environment. Locked after initialization.
+                        processDataEnv = "environment",
+                        
+                        ## A vector c(a,b) with the support, [a,b], of the g-functions.
+                        support = "numeric",     
+
                         "VIRTUAL")
          )
 
+setMethod("computeMinusLogLikelihood","PointProcess",
+          function(model,coefficients=NULL,...){
+            eta <- computeLinearPredictor(model,coefficients,...)
+             if(attr(terms(model@formula),"response") != 0) {
+              response <- all.vars(model@formula,unique=FALSE)[attr(terms(model@formula),"response")]
+            } else stop("no response variable specified")
+            
+
+            if(model@family@link == "log"){
+              mll <-  sum(exp(eta)*model@delta) -
+                sum(eta[getMarkTypePosition(getProcessData(model),response)]) 
+            } else {
+              mll <-  sum(model@family@phi(eta)*model@delta) -
+                sum(log(model@family@phi(eta[getMarkTypePosition(getProcessData(model),response)]))) 
+            }
+            
+            return(mll)
+            
+          }
+          )
 
 setMethod("formula","PointProcess",
           function(x,...){
@@ -32,25 +63,16 @@ setMethod("getProcessData","PointProcess",
           }
           )
 
-setMethod("computeMinusLogLikelihood","PointProcess",
-          function(model,coefficients=NULL,...){
-            eta <- computeLinearPredictor(model,coefficients,...)
-             if(attr(terms(model@formula),"response") != 0) {
-              response <- all.vars(model@formula,unique=FALSE)[attr(terms(model@formula),"response")]
-            } else stop("no response variable specified")
-            
-
-            if(model@family@link == "log"){
-              mll <-  sum(exp(eta)*model@delta) -
-                sum(eta[getMarkTypePosition(getProcessData(model),response)]) 
-            } else {
-              mll <-  sum(model@family@phi(eta)*model@delta) -
-                sum(log(model@family@phi(eta[getMarkTypePosition(getProcessData(model),response)]))) 
-            }
-            
-            return(mll)
-            
+setReplaceMethod("setProcessData",c(model="PointProcess",value="ProcessData"),
+          function(model,value){
+            processDataEnv <- new.env(parent=.GlobalEnv)
+            processDataEnv$processData <- value
+            lockEnvironment(processDataEnv,bindings=TRUE)
+            model@processDataEnv <- processDataEnv
+            return(model)
           }
           )
+
+
 
 
