703cad4fbdf5496d47b429c98893437efc87488b
nielsrhansen <nielsrhansen@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-05 22:13:00 +0000

    Introduction of multiscale bases in 'bSpline'. Some small modifications in 'summary' methods, and 'affine' function and two internal functions added.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ppstat@79 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ppstat.R
+++ pkg/R/ppstat.R
@@ -15,49 +15,92 @@ lapplyParallel <- function(...) .ppstatGlobals$lapplyParallel(...)
 ## evaluated outside of knots. This will in fact be the rule rather
 ## than the exception for point process usages.
 
-bSpline <- function(x, knots, ..., sym = FALSE, trunc = NULL) {
-  if(length(knots) <= 4) stop("Needs at least 5 knots.")
-  if(sym) {
-    design <- list()
-    
-    if(any(x>=0))
-      design[[1]] <- bSpline(x = x[x>=0], knots = knots, ..., trunc = NULL, sym = FALSE)
-
-    if(any(x < 0))
-      design[[2]] <- bSpline(x = -x[x<0], knots = knots, ..., trunc = NULL, sym = FALSE)
-    design <- do.call("rbind", design)
-  } else {
-    design <- splineDesign(knots, x, ..., outer.ok=TRUE)
-  }
-  if(!is.null(trunc)) {
-    if(length(trunc) == 1) {
-      design[x <= trunc, ] <- 0
-    } else if(length(trunc) == 2) {
-      design[x <= trunc[1] | x > trunc[2], ] <- 0
+bSpline <- function(x, knots, ..., sym = FALSE, trunc = NULL, levels = NULL) {
+  if(is.null(levels)) {
+    if(length(knots) <= 4) 
+      stop("Needs at least 5 knots.")
+    if(sym) {
+      design <- list()
+      
+      if(any(x>=0))
+        design[[1]] <- bSpline(x = x[x>=0], knots = knots, ..., trunc = NULL, sym = FALSE)
+      
+      if(any(x < 0))
+        design[[2]] <- bSpline(x = -x[x<0], knots = knots, ..., trunc = NULL, sym = FALSE)
+      design <- do.call("rbind", design)
     } else {
-      stop("The truncation argument 'trunc' must be either a single numeric or a vector of length 2.")
+      design <- splineDesign(knots, x, ..., outer.ok=TRUE)
     }
+    if(!is.null(trunc)) {
+      if(length(trunc) == 1) {
+        design[x <= trunc, ] <- 0
+      } else if(length(trunc) == 2) {
+        design[x <= trunc[1] | x > trunc[2], ] <- 0
+      } else {
+        stop("The truncation argument 'trunc' must be either a single numeric or a vector of length 2.")
+      }
+    }
+    design <- design[ , apply(design, 2, function(s) any(s != 0))]
+  } else {
+    if(!is.numeric(levels) || levels < 1) 
+      stop("For multiscale modeling, 'levels' must be an integer larger than or equal to 1.")
+    nrKnots <- 2^levels * 4 + 1
+    lower <- knots[1]
+    upper <- knots[length(knots)]
+    if(length(knots) == 1)
+      lower <- 0
+    knots <- seq(lower, upper, length.out = nrKnots)
+    design <- list()
+    kn1 <- knots[seq(1, nrKnots, 2^levels)]
+    kn1 <- c(3 * kn1[1] - 2 * kn1[2], 2 * kn1[1] - kn1[2], kn1)
+    kn2 <- knots[seq(1 + 2^(levels - 1), nrKnots, 2^levels)]
+    kn2 <- c(3 * kn2[1] - 2 * kn2[2], 2 * kn2[1] - kn2[2], kn2)
+    design[[1]] <- cbind(bSpline(x, knots = kn1, ...,
+                                 trunc = NULL, sym = FALSE),
+                         bSpline(x, knots = kn2, ...,
+                                     trunc = NULL, sym = FALSE)
+                         )
+    if(levels > 1) {
+      for (k in seq(2, levels)) {
+        kn = knots[seq(1 + 2^(levels - k), nrKnots, 2^(levels - k + 1))]
+        kn <- c(2 * kn[1] - kn[2], 2 * kn[1] - kn[2], kn)
+        design[[k]] <- bSpline(x, knots = kn, ...,
+                                   trunc = NULL, sym = FALSE)
+      }
+    }    
+    design <- do.call("cbind", design)
   }
-  design <- design[ , apply(design, 2, function(s) any(s != 0))]
   return(design)
 }
 
 ## Truncated exponential
 
-tExp <- function(x, tLevel=1e-4){
+tExp <- function(x, tLevel = 1e-4) {
   y <- exp(x)
   y[y < tLevel] <- 0
   return(y)
 }
 
-## Constant function
+## Safe logarithm
+
+safeLog <- function(x) {
+  x[x < 0] <- 0
+  log(x)
+}
+
+## Constant and affine functions
 
 const <- function(x, y, c = 1){
-  lesseq <- rep(TRUE, length(x))
-  if(!missing(y))
+  if(missing(y)) {
+    lesseq <- rep(1, length(x))
+  } else {
     lesseq <- (x <= y)
+  }
+  c * as.numeric(lesseq)
+}
 
-  return(c*as.numeric(lesseq))
+affine <- function(x, a = 1, b = 1) {
+    matrix(c(b * rep(1, length(x)), a * x), ncol = 2)
 }
 
 ### TODO: This should be changed to an S4 method and specialized to the
@@ -73,12 +116,6 @@ print.summary.ppm <-  function (x, digits = max(3, getOption("digits") - 3),
     coefs <- x$coefficients
     printCoefmat(coefs, digits = digits, signif.stars = signif.stars, 
             na.print = "NA", ...)
-    if(length(x$fixedCoefficients$which) == 1) {
-      cat("\nNote that", rownames(x$coefficients)[x$fixedCoefficients$which], "was fixed\n\n")
-    } else if(length(x$fixedCoefficients$which) > 1) {
-      cat("\nNote that", paste(rownames(x$coefficients)[x$fixedCoefficients$which],collapse=", "), "were fixed\n\n")
-    }
-    if(x$penalization) cat("\nWarning: Parameters estimated with a quadratic penalty term.\nCorrections of the degrees of freedom is currently not implemented\nand AIC is not corrected either. The estimated standard errors may be\ninaccurate and the univariate test statistics can be misleading.\n\n")
     cat("\nMinus-log-likelihood: ", format(x$mll, digits = max(4, digits + 1)), " on ",x$df, " degrees of freedom",
         "\n", sep = "")
     cat("AIC: ", format(x$aic, digits = max(4, digits + 1)), 
@@ -94,4 +131,8 @@ print.summary.ppm <-  function (x, digits = max(3, getOption("digits") - 3),
     invisible(x)
 }
 
+## Special functions used to construct model matrices
+
+.__s__ <- function(t) cbind(1, t)
+.__k__ <- function(t) t
 
